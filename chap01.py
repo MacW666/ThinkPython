1ba0d89b4b534aa7bbdb1a0a18305b71a09ed5a0
MacW666 <76020870+MacW666@users.noreply.github.com>
2022-10-19 11:51:23 +0800

    Add files via upload

---
+++ chap01.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 11:11:48 2022
+
+@author: wx
+"""
+
+#1
+print('hello BNU')
+#print('hello'   unexpected EOF while parsing
+#print 'hello'    Missing parentheses in call to 'print'. Did you mean print('hello')?
+
+#2
+#print(hello)    name 'hello' is not defined
+#print('hello)    EOL while scanning string literal
+
+#3
+print(-2)
+print(2++2)
+print(2+2)
+
+#4
+#print(02)    leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+
+#5
+#print(2 3)     invalid syntax
